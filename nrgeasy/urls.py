01be70edb4e3f40eb533634e25a7eba670f24d2e
Helton França <144186013+HeltonFranca@users.noreply.github.com>
2023-10-11 18:12:12 -0300

    Add files via upload

--- nrgeasy/urls.py
+++ nrgeasy/urls.py
@@ -6,5 +6,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/',include("clientes.urls")),
+    path('', include('home.urls')), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
